0e7a2f9d8ce08198df1229b95c1ed2bce711993b
Maria Z <z0559882272@gmail.com>
2024-06-27 20:02:16 +0300

    login negative

--- src/main/java/screens/ContactListScreen.java
+++ src/main/java/screens/ContactListScreen.java
@@ -3,8 +3,9 @@ package screens;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
-public class ContactListScreen extends BaseScreen{
+public class ContactListScreen extends BaseScreen {
     public ContactListScreen(AppiumDriver<AndroidElement> driver) {
         super(driver);
     }
@@ -12,11 +13,31 @@ public class ContactListScreen extends BaseScreen{
     @FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/action_bar']/android.widget.TextView")
     AndroidElement activityTextView;
 
+    @FindBy(xpath = "//*[@content-desc='More options']")
+    AndroidElement menuOption;
 
-    public boolean isActivityTitleDisplayed(String text){
+    @FindBy(xpath = "//*[@text='Logout']")
+    AndroidElement logoutBtn;
+
+
+    public boolean isActivityTitleDisplayed(String text) {
 
         //return activityTextView.getText().contains("Contact list");
-        return isShouldHave(activityTextView,text,10);
+        return isShouldHave(activityTextView, text, 10);
+    }
+
+
+    public AuthenticationScreen logout() {
+        if(activityTextView.getText().equals("Contact list")) {
+            menuOption.click();
+            logoutBtn.click();
+        }
+        return new AuthenticationScreen(driver);
+    }
+
+    public ContactListScreen isAccountOpened(){
+        Assert.assertTrue(isActivityTitleDisplayed("Contact list"));
+        return this;
     }
 
 }
